72a1ca3bf1a92004d38799209dfd6727aeff364b
unknown <rohit kadam@DESKTOP-8DQ9U7K>
2025-03-29 13:08:42 +0530

    hi

--- models/worker.js
+++ models/worker.js
@@ -1,19 +1,7 @@
 const express=require(`express`)
 
-const PORT=8000
-
-const app=express()
-
-const mongoose=require(`mongoose`);
-const { log } = require("node:console");
-
-
 // const mongopass=ahcREmAWmQyB9XR4;
 // const newmongopass=useradmin123;
-
-
-mongoose.connect(`mongodb+srv://admin121:useradmin123@cluster0.1rhazaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-
 
 
 const schema=mongoose.Schema({
@@ -36,10 +24,3 @@ const schema=mongoose.Schema({
 })
 
 const workermodel=mongoose.model(`Worker`,schema)
-
-app.use
-
-app.listen(PORT,()=>{
-    console.log(`Server is live on ${PORT}`);
-    
-})
