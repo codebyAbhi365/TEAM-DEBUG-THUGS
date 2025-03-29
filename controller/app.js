2c8a57635288663823ff6531d2ed4802b1c4d0aa
unknown <rohit kadam@DESKTOP-8DQ9U7K>
2025-03-30 02:57:49 +0530

    add

--- controller/app.js
+++ controller/app.js
@@ -3,18 +3,6 @@ const User = require("../models/worker");
 const { getUser } = require("../service/auth");
 const Accepted = require("../models/Accepted"); // Import the Accepted model
 
-<<<<<<< HEAD
-async function filecomplain(req, res) {
-    try {
-        const { Name, Location, Image } = req.body; // Fixed destructuring
-        await complaindata.create({ Name, Location, Image });
-        res.redirect(`/homepage`);
-    } catch (error) {
-        console.error("Error filing complaint:", error);
-        res.status(500).send("Internal Server Error");
-    }
-}
-=======
 // async function filecomplain (req, res){
 //     const {Name,Location,Image}=await req.body
 //     await complaindata.create({
@@ -26,7 +14,6 @@ async function filecomplain(req, res) {
 //     console.log(Name);
 //     res.redirect(`/homepage`)
 // }
->>>>>>> a52740825fe61ba36167396ee22181076850724f
 
 // Function to show user profile
 async function showProfile(req, res) {
@@ -34,6 +21,7 @@ async function showProfile(req, res) {
         const userUid = req.cookies?.uid;
         if (!userUid) return res.redirect("/login");
 
+        
         const user = getUser(userUid);
         if (!user) return res.redirect("/login");
 
@@ -46,11 +34,6 @@ async function showProfile(req, res) {
 }
 
 module.exports = {
-<<<<<<< HEAD
-    showProfile,
-    filecomplain,
-};
-=======
    showProfile
    
 } 
@@ -58,4 +41,3 @@ module.exports = {
  
 //  Mainpage,
 // ,filecomplain,
->>>>>>> a52740825fe61ba36167396ee22181076850724f
