a5d09f37a65c066b24b5042ed8bbdde6c2015268
unknown <rohit kadam@DESKTOP-8DQ9U7K>
2025-03-29 18:47:56 +0530

    add

--- controller/user.js
+++ controller/user.js
@@ -4,23 +4,35 @@ const Worker = require("../models/worker")
 
 async function createUser(req, res){
     const {Name,Email,PhnNo,Pass}=req.body;
-    Worker.create({
+
+    const existingUser = await Worker.findOne({ Email });
+    if (existingUser) {
+        return res.render("signup", { error: "Email already registered" });
+    }
+
+    await Worker.create({
         Name,
         Email,
         PhnNo,
-        Pass
+        Pass: hashedPassword
     });
+
     console.log(Name);
     res.redirect(`/home`);
 }
 
 async function loginUser(req, res){
     const {Email, Pass} = req.body;
-    const user = await Worker.findOne({Email , Pass});
+    const user = await Worker.findOne({Email});
 
     if(!user) return res.render("login" , {
         error: "Invalid Password or Username"
     })
+
+    const isMatch = await bcrypt.compare(Pass, user.Pass);
+    if (!isMatch) {
+        return res.render("login", { error: "Invalid Email or Password" });
+    }
 
     const sessionId = uuidv4();
     res.cookie("uid", sessionId);
@@ -29,12 +41,7 @@ async function loginUser(req, res){
     return res.redirect("/home");
 }
 
-
-
-
-
-
-
 module.exports = {
-    createUser,loginUser,
+    createUser,
+    loginUser,
 }
