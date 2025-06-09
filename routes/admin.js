2a1ccb365f37b1daab97b91d1b51f82393c5d131
unknown <rohit kadam@DESKTOP-8DQ9U7K>
2025-06-09 23:10:11 +0530

    add

--- routes/admin.js
+++ routes/admin.js
@@ -14,8 +14,22 @@ adminRouter.post('/approve/:id', async (req, res) => {
 });
 
 adminRouter.get("/", async(req, res)=>{
-  const complaints = await complainmodel.find({ status: 'pending' });
-    return res.render("verifyImage.ejs",{complaints});
+  const complaints = await complainmodel.find();
+  rejected_count = 0;
+  approved_count = 0;
+  pending_count = 0;
+  complaints.forEach(complaint => {
+    if(complaint.status === "rejected"){
+      rejected_count += 1; 
+    }
+    if(complaint.status === "approved"){
+      approved_count += 1; 
+    }
+    if(complaint.status === "pending"){
+      pending_count += 1; 
+    }
+  });
+    return res.render("verifyImage.ejs",{complaints,rejected_count,approved_count,pending_count});
 })
 
 adminRouter.delete('/reject/:id', async (req, res) => {
